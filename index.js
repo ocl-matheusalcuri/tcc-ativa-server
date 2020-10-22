462f3c2f1bcacb7acf45b1296db71452460d6a2c
matheus-alcuri <matheusalcuri2@gmail.com>
2020-10-22 12:13:38 -0300

    removendo mais comentários

--- index.js
+++ index.js
@@ -39,26 +39,15 @@ app.use(authMiddleware);
 app.post("/upload", async (req, res) => {
 
   const {base64, userId, type} = req.body.body;
-
-  // Configure AWS with your access and secret key.
   const { ACCESS_KEY_ID, SECRET_ACCESS_KEY, AWS_REGION, S3_BUCKET } = process.env;
 
-  // Configure AWS to use promise
   AWS.config.setPromisesDependency(require('bluebird'));
   AWS.config.update({ accessKeyId: ACCESS_KEY_ID, secretAccessKey: SECRET_ACCESS_KEY, region: AWS_REGION });
 
-  // Create an s3 instance
   const s3 = new AWS.S3();
-
-  // Ensure that you POST a base64 data to your server.
-  // Let's assume the variable "base64" is one.
   const base64Data = new Buffer.from(base64.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+  const mimeInfo = fileType(Buffer.from(base64, 'base64'));
 
-  const mimeInfo = fileType(Buffer.from(base64, 'base64'))
-
-  // With this setup, each time your user uploads an image, will be overwritten.
-  // To prevent this, use a different Key each time.
-  // This won't be needed if they're uploading their avatar, hence the filename, userAvatar.js.
   const params = {
     Bucket: S3_BUCKET,
     Key: `${userId}_${Date.now()}.${mimeInfo.ext}`, // type is not required
@@ -68,8 +57,6 @@ app.post("/upload", async (req, res) => {
     ContentType: `${mimeInfo.mime}` // required. Notice the back ticks
   }
 
-  // The upload() is used instead of putObject() as we'd need the location url and assign that to our user profile/database
-  // see: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#upload-property
   let location = '';
   let key = '';
   try {
@@ -80,11 +67,7 @@ app.post("/upload", async (req, res) => {
     console.log({error})
   }
   
-  // Save the Location (url) to your database and Key if needs be.
-  // As good developers, we should return the url and let other function do the saving to database etc
-
   const user = type === "aluno" ? await Aluno.findById(userId) : await Personal.findById(userId);
-
   user.fotoUrl = location;
   user.temFoto = true;
   await user.save();
@@ -93,7 +76,7 @@ app.post("/upload", async (req, res) => {
       user: {name: user.nome, email: user.email, temFoto: user.temFoto, userFoto: user.fotoUrl},
       url: location
   });
-  });
+});
 
 app.use('/api/agendaModel', require('./api/Agenda'))
 app.use('/api/alunoModel', require('./api/Aluno'))
